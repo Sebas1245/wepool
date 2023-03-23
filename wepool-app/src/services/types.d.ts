5707e3384f2d938396af7647e2298e6f537520a4
BernardoG <A00570682@itesm.mx>
2023-03-23 17:41:24 -0600

    Updated types from prisma generated models

--- wepool-app/src/services/types.d.ts
+++ wepool-app/src/services/types.d.ts
@@ -1,47 +1,74 @@
 declare global{
     
     type User = {
-        _id: number, 
-        name: string, 
-        lastname: string, 
-        email: string,
-        password: string,
-        phoneNumber: string,
-    }
-
-    type Driver = {
-        _id: number, 
-        name: string, 
-        lastname: string, 
-        home_latitude: number, 
-        home_longitude: number,
-    }
-
-    type Rider = {
-        _id: number, 
-        name: string, 
-        lastname: string, 
-        home_latitude: number, 
-        home_longitude: number,
+        id: number;
+        createdAt: Date;
+        updatedAt: Date;
+        fname: string;
+        lname: string;
+        latitude?: number | null;
+        longitude?: number | null;
+        street?: string | null;
+        number?: number | null;
+        zipCode?: number | null;
+        city?: string | null;
+        state?: string | null;
+        country?: string | null;
+        car?: Car | null;
+        carId?: number | null;
+        driverRides?: Ride[];
+        passengerRides?: RidePassengers[];
+        company?: Company;
+        companyId: number;
+        _count?: UserCount | null;
     }
 
     type Ride = {
-        _id: number, 
-        driver_id: Driver._id,
-        driver: Driver,
-        available_seats: number,
-        status: string,
+        id: number;
+        kmToGoalLocation: number;
+        availableSeats: number;
+        status: "OPEN" | "CLOSED";
+        startsAt: "DRIVER" | "COMPANY";
+        driver?: User;
+        driverId: number;
+        passengers?: RidePassengers[];
+        _count?: RideCount | null;
     }
 
     type Car = {
-        _id: number, 
-        driver_id: Driver._id,
-        driver: Driver,
-        capacity: number,
-        brand: string,
-        model: string,
-        year: number,
-        plate_number: string,
+        id: number;
+        brand: string;
+        model: string;
+        year: number;
+        plateNumber: string;
+        capacity: number;
+        color: string;
+        driver?: User;
+        driverId: number;
+    }
+
+    type Company = {
+        id: number;
+        name: string;
+        latitude?: number | null;
+        longitude?: number | null;
+        street: string;
+        number: number;
+        zipCode: number;
+        city: string;
+        state: string;
+        country: string;
+        users?: User[];
+        _count?: CompanyCount | null;
+    }
+
+    type RidePassengers = {
+        id: number;
+        createdAt: Date;
+        passenger?: User;
+        passengerId: number;
+        ride?: Ride;
+        rideId: number;
     }
 }
 
