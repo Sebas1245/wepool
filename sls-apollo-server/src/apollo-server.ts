50189b95de9ba2721be1506ed76fd8f82c64e527
Sebastian Saldaña <ssaldanacardenas@gmail.com>
2023-03-24 18:35:28 -0600

    Delete old files before merge

--- sls-apollo-server/src/apollo-server.ts
+++
@@ -1,7 +0,0 @@
-import { ApolloServer } from "apollo-server-lambda";
-import { resolvers } from "./resolvers";
-import { typeDefs } from "./types/type-defs";
-
-const apolloServer = new ApolloServer({ typeDefs , resolvers });
-
-export const graphqlServer = apolloServer.createHandler();
